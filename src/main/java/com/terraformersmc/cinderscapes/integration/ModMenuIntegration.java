b4ea468b9e0fa888dfae105adc384850f27d52c9
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-05-08 15:02:14 -0400

    commented out modMenu integration stuff

--- src/main/java/com/terraformersmc/cinderscapes/integration/ModMenuIntegration.java
+++ src/main/java/com/terraformersmc/cinderscapes/integration/ModMenuIntegration.java
@@ -1,14 +1,14 @@
 package com.terraformersmc.cinderscapes.integration;
 
-import com.terraformersmc.cinderscapes.config.CinderscapesConfig;
+/*import com.terraformersmc.cinderscapes.config.CinderscapesConfig;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
+import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;*/
 
-public class ModMenuIntegration implements ModMenuApi {
+public class ModMenuIntegration /*implements ModMenuApi*/ {
 
-    @Override
+    /*@Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return parent -> AutoConfig.getConfigScreen(CinderscapesConfig.class, parent).get();
-    }
+    }*/
 }
