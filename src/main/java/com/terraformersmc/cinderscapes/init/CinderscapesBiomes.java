1d63013825f1b3acca5eb0262709792360a22162
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-08-24 23:19:28 -0400

    added nebi dep

--- src/main/java/com/terraformersmc/cinderscapes/init/CinderscapesBiomes.java
+++ src/main/java/com/terraformersmc/cinderscapes/init/CinderscapesBiomes.java
@@ -5,6 +5,8 @@ import com.terraformersmc.cinderscapes.biome.AshyShoalsBiome;
 import com.terraformersmc.cinderscapes.biome.BlackstoneShalesBiome;
 import com.terraformersmc.cinderscapes.biome.LuminousGroveBiome;
 import com.terraformersmc.cinderscapes.biome.QuartzCanyonBiome;
+import com.terraformersmc.cinderscapes.config.CinderscapesConfig;
+import net.fabricmc.fabric.api.biome.v1.NetherBiomes;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
@@ -15,16 +17,18 @@ import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.function.Supplier;
+
 public class CinderscapesBiomes {
 
     // Acts as a kind of local registry for Cinderscape's custom biomes
 
     public static final DeferredRegister<Biome> BIOMES = DeferredRegister.create(ForgeRegistries.BIOMES, Cinderscapes.ID);
 
-    public static final RegistryObject<Biome> BLACKSTONE_SHALES = add("blackstone_shales", BlackstoneShalesBiome.create());
-    public static final RegistryObject<Biome> QUARTZ_CANYON = add("quartz_canyon", QuartzCanyonBiome.create());
-    public static final RegistryObject<Biome> LUMINOUS_GROVE = add("luminous_grove", LuminousGroveBiome.create(), BiomeDictionary.Type.FOREST);
-    public static final RegistryObject<Biome> ASHY_SHOALS = add("ashy_shoals", AshyShoalsBiome.create(), BiomeDictionary.Type.SANDY);
+    public static final RegistryObject<Biome> BLACKSTONE_SHALES = add("blackstone_shales", BlackstoneShalesBiome::create);
+    public static final RegistryObject<Biome> QUARTZ_CANYON = add("quartz_canyon", QuartzCanyonBiome::create);
+    public static final RegistryObject<Biome> LUMINOUS_GROVE = add("luminous_grove", LuminousGroveBiome::create, BiomeDictionary.Type.FOREST);
+    public static final RegistryObject<Biome> ASHY_SHOALS = add("ashy_shoals", AshyShoalsBiome::create, BiomeDictionary.Type.SANDY);
 
     // Required because something referencing this class has to be called in order to force the
     // static initializer to run (Minecraft's Bootstrap class does similar things)
@@ -32,8 +36,7 @@ public class CinderscapesBiomes {
     }
 
     public static void initBiomeAdd(){
-        //Todo fabric change, will be fixed via nebi
-        /*if (CinderscapesConfig.BIOME_OPTIONS.ENABLE_ASHY_SHOALS) {
+        if (CinderscapesConfig.BIOME_OPTIONS.ENABLE_ASHY_SHOALS) {
             NetherBiomes.addNetherBiome(RegistryKey.of(Registry.BIOME_KEY, Cinderscapes.id("ashy_shoals")), AshyShoalsBiome.NOISE_POINT);
         }
         if (CinderscapesConfig.BIOME_OPTIONS.ENABLE_LUMINOUS_GROVE) {
@@ -44,21 +47,21 @@ public class CinderscapesBiomes {
         }
         if (CinderscapesConfig.BIOME_OPTIONS.ENABLE_BLACKSTONE_SHALES) {
             NetherBiomes.addNetherBiome(RegistryKey.of(Registry.BIOME_KEY, Cinderscapes.id("blackstone_shales")), BlackstoneShalesBiome.NOISE_POINT);
-        }*/
+        }
     }
 
-    private static RegistryObject<Biome> add(String s, Biome b) {
+    private static RegistryObject<Biome> add(String s, Supplier<Biome> b) {
         Identifier id = Cinderscapes.id(s);
         RegistryKey<Biome> key = RegistryKey.of(Registry.BIOME_KEY, id);
         BiomeDictionary.addTypes(key, new BiomeDictionary.Type[]{BiomeDictionary.Type.NETHER, BiomeDictionary.Type.HOT, BiomeDictionary.Type.DRY});
-        return BIOMES.register(id.toString(), () -> b);
+        return BIOMES.register(id.toString(), b);
     }
 
-    private static RegistryObject<Biome> add(String s, Biome b, BiomeDictionary.Type special) {
+    private static RegistryObject<Biome> add(String s, Supplier<Biome> b, BiomeDictionary.Type special) {
         Identifier id = Cinderscapes.id(s);
         RegistryKey<Biome> key = RegistryKey.of(Registry.BIOME_KEY, id);
         BiomeDictionary.addTypes(key, new BiomeDictionary.Type[]{BiomeDictionary.Type.NETHER, BiomeDictionary.Type.HOT, BiomeDictionary.Type.DRY, special});
-        return BIOMES.register(id.toString(), () -> b);
+        return BIOMES.register(id.toString(), b);
     }
 
 }
