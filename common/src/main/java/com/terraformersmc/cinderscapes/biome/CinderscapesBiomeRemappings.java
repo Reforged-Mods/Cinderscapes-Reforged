f1d95967b7b1e72a67551ba4b345d363294d07cf
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-10-09 02:03:00 -0400

    fixed remapping event

--- common/src/main/java/com/terraformersmc/cinderscapes/biome/CinderscapesBiomeRemappings.java
+++ common/src/main/java/com/terraformersmc/cinderscapes/biome/CinderscapesBiomeRemappings.java
@@ -3,12 +3,16 @@ package com.terraformersmc.cinderscapes.biome;
 import com.terraformersmc.cinderscapes.Cinderscapes;
 import com.terraformersmc.cinderscapes.init.CinderscapesBiomes;
 import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
 public class CinderscapesBiomeRemappings {
-	private static void onRemap(RegistryEvent.MissingMappings<Biome> event){
+	public static void onRemap(RegistryEvent.MissingMappings<Biome> event){
+		Cinderscapes.LOGGER.info("Missing mappings");
 		for (RegistryEvent.MissingMappings.Mapping<Biome> map : event.getMappings(Cinderscapes.NAMESPACE)) {
+			Cinderscapes.LOGGER.info(map.key);
 			if (map.key.getPath().equals("quartz_canyon")){
 				Biome[] biome = new Biome[1];
 				CinderscapesBiomes.BIOMES.forEach((k, v) -> {
@@ -21,7 +25,7 @@ public class CinderscapesBiomeRemappings {
 		}
 	}
 
-	public static void init() {
-		FMLJavaModLoadingContext.get().getModEventBus().addGenericListener(Biome.class, CinderscapesBiomeRemappings::onRemap);
+	public static void init(){
+		MinecraftForge.EVENT_BUS.addGenericListener(Biome.class, CinderscapesBiomeRemappings::onRemap);
 	}
 }
