387ac2834584d36c0f9381e74c63e64ccd6fa039
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-05-21 23:49:40 -0400

    commented out biome remapping

--- common/src/main/java/com/terraformersmc/cinderscapes/biome/CinderscapesBiomeRemappings.java
+++ common/src/main/java/com/terraformersmc/cinderscapes/biome/CinderscapesBiomeRemappings.java
@@ -1,16 +1,9 @@
 package com.terraformersmc.cinderscapes.biome;
 
-import com.google.common.collect.ImmutableMap;
-import com.terraformersmc.cinderscapes.Cinderscapes;
-import com.terraformersmc.terraform.biomeremapper.api.BiomeRemapper;
-import com.terraformersmc.terraform.biomeremapper.api.BiomeRemapperApi;
-import com.terraformersmc.terraform.biomeremapper.api.DataVersions;
-
-@BiomeRemapper
-public class CinderscapesBiomeRemappings implements BiomeRemapperApi {
+public class CinderscapesBiomeRemappings {
 	public void init() {
-		register(Cinderscapes.NAMESPACE, DataVersions.V_1_18_2, ImmutableMap.<String, String>builder()
+		/*register(Cinderscapes.NAMESPACE, DataVersions.V_1_18_2, ImmutableMap.<String, String>builder()
 				.put("cinderscapes:quartz_canyon", "cinderscapes:quartz_cavern")
-				.build());
+				.build());*/
 	}
 }
